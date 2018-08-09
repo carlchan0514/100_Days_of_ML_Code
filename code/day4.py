cd4740731fa7d9392bcd46ad5e95d5c778a9b21b
chendong-roadstar <chendong@roadstar.ai>
2018-08-09 13:56:48 +0800

    add the plot decision boundary lib

--- code/day4.py
+++ code/day4.py
@@ -53,8 +53,47 @@ Y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test, Y_pred)
 
+# excersize
+# if "python setup.py develop" installed the package
+from pylib.plot import plot_decision_boundary
+
+# generate some data from sklearn
+from sklearn import datasets, linear_model, tree, ensemble
+# matplotlib inline
+X, Y = datasets.make_blobs(centers=6, random_state=11)
+# print(X)
+# print(Y)
+
+# Now let's start visualizing logistic regression
+model = linear_model.LogisticRegression()
+model.fit(X, Y)
+plot_decision_boundary(model, X=X, Y=Y)
+plt.show()
+
+# take a look at decision tree
+model = tree.DecisionTreeClassifier()
+model.fit(X, Y)
+plot_decision_boundary(model, X=X, Y=Y)
+plt.show()
+
+# how about random forest?
+model = ensemble.RandomForestClassifier(n_estimators=21, random_state=1)
+model.fit(X, Y)
+plot_decision_boundary(model, X=X, Y=Y)
+plt.show()
+
+# higher dimension case
+data = datasets.load_iris()
+X = data.data
+Y = data.target
+
+model = linear_model.LogisticRegression()
+model.fit(X, Y)
+plot_decision_boundary(model, X=X, Y=Y)
+plt.show()
+
 # Visualization
-#Visualize the Training Set results
+# Visualize the Training Set results
 x_set, y_set = X_train, Y_train
 x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min()-1, stop=x_set[:, 0].max()+1, step=0.01),
                      np.arange(start=x_set[:, 1].min()-1, stop=x_set[:, 1].max()+1, step=0.01))
@@ -69,7 +108,7 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.show()
 
-#Visualize the Test Set Results
+# Visualize the Test Set Results
 x_set, y_set = X_test, Y_test
 x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min()-1, stop=x_set[:, 0].max()+1, step=0.01),
                      np.arange(start=x_set[:, 1].min()-1, stop=x_set[:, 1].max()+1, step=0.01))
@@ -82,4 +121,9 @@ for i, j in enumerate(np.unique(y_set)):
 plt.title('Logistic Regression (Testing set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
+plt.show()
+
+# Now let's visualize logistic regression
+plot_decision_boundary(classifier, X=X_train, Y=Y_train)
+plot_decision_boundary(classifier, X=X_test, Y=Y_test)
 plt.show()
