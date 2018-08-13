db863284c3f05cf2b7f9aa63f1417e92f2292a8a
chendong-roadstar <chendong@roadstar.ai>
2018-08-13 19:15:55 +0800

    day 8 of 100 days of ML Code
    day 6 and day 7 went to the seaside for entertainment.

--- code/day5.py
+++ code/day5.py
@@ -1,16 +1,13 @@
-# Step 1 | Data Pre-Processing
-# Importing the Libraries
+# Importing the libraries
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv("../datasets/Social_Network_Ads.csv")
-X = dataset.iloc[:, [2, 3]].values
-Y = dataset.iloc[:, 4].values
-# print(X)
-# print(Y)
+dataset = pd.read_csv('../datasets/Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+Y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -22,32 +19,13 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Step 2 | Logistic Regression Model
-
-# The library for this job which is going to be the linear model library
-# and it is called linear because the logistic regression is a linear classifier
-# which means that here since we are in two dimensions,
-# our two categories of users are going to be separated by a straight line.
-# Then import the logistic regression class.
-# Next we will create a new object from this class which is going to be our classifier
-# that we are going to fit on our training set.
-
-# Fitting Logistic Regression to the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression()
+# Fitting K-NN to the Training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=2, metric='minkowski', p=2)
 classifier.fit(X_train, Y_train)
 
-# Step 3 | Predection
 # Predicting the Test set results
 Y_pred = classifier.predict(X_test)
-
-# Step 4 | Evaluating The Predection
-
-# We predicted the test results and now we will evaluate
-# if our logistic regression model learned and understood correctly.
-# So this confusion matrix is going to contain the correct predictions
-# that our model made on the set as well as the incorrect predictions.
-# here is some material about confusion matrix https://blog.csdn.net/joeland209/article/details/71078935
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
@@ -127,3 +105,5 @@ plt.show()
 # plot_decision_boundary(classifier, X=X_train, Y=Y_train)
 # plot_decision_boundary(classifier, X=X_test, Y=Y_test)
 # plt.show()
+
+print(Y_pred)
